c546f8f91c66bff29890ca609b3c5b0fcf64836b
Shreyansh Kumar <shreyansh.k@myplanet.com>
2022-09-30 04:02:48 -0400

    Created Poke API

---
+++ README.md
@@ -0,0 +1,7 @@
+# Poke API
+This is a Node API to support Poke App. It connects to a Mongo Cluster hosted on Mongo Atlas. It is used to sync pokemon data from the Pokemon API to the Mongo database
+
+## Quick Start
+1. `git clone`
+2. `yarn` or `npm i` to install dependencies
+3. `yarn start` to start your local dev server
---
+++ index.ts
@@ -0,0 +1,88 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { MongoClient } = require('mongodb')
+
+import {Request, Response} from 'express'
+const app = express()
+app.use(cors())
+
+const jsonParser = bodyParser.json()
+
+const MONGO_CONNECTION_URI = 'mongodb+srv://shreyanshkumar:8809asAS@cluster0.ieg8mm3.mongodb.net/test'
+
+interface Pokemon {
+  image_url: string,
+  types: string[],
+  name: string
+}
+
+const mongoClient = new MongoClient(MONGO_CONNECTION_URI)
+
+const connectDb = async () => {
+  try {
+    await mongoClient.connect()
+    await mongoClient.db('admin').command({ ping: 2 })
+
+    console.log('Successfully established connection')
+    return mongoClient.db('test')
+  } catch (error) {
+    throw new Error(`Error connecting to Mongo: ${error}`)
+  }
+}
+
+const disconnectDb = async () => {
+  await mongoClient.close()
+  console.log('Successfully disconnected')
+}
+
+app.get('/', (req: Request, res: Response) => {
+  res.json('OK').status(200)
+})
+
+app.post('/pokemon/sync', jsonParser, async (req: Request, res: Response) => {
+  const { allPokemon }: { allPokemon: Pokemon[] } = req.body
+  
+  const db = await connectDb()
+  const mongoResponse = await db.collection('pokemon').insertMany(allPokemon)
+  res.json(mongoResponse).status(200)
+})
+
+app.get('/pokemon', async (req: Request, res: Response) => {
+  const db = await connectDb()
+
+  const allPokemon = await db.collection('pokemon').find().toArray()
+
+  await disconnectDb()
+
+  res.json(allPokemon).status(200)
+})
+
+app.post('/pokemon', jsonParser, async (req: Request, res: Response) => {
+  const { pokemon }: { pokemon: Pokemon } = req.body
+
+  const db = await connectDb()
+  const mongoResponse = await db.collection('pokemon').insertOne(pokemon)
+  await disconnectDb()
+
+  res.json(mongoResponse).status(200)
+})
+
+app.delete('/pokemon/:name', jsonParser, async (req: Request, res: Response) => {
+  const { name } = req.params
+  const db = await connectDb()
+  const mongoResponse = await db.collection('pokemon').findOneAndDelete({ name })
+  await disconnectDb()
+
+  res.json(mongoResponse).status(200)
+})
+
+app.post('/disconnect', async (req: Request, res: Response) => {
+  await disconnectDb()
+  res.send('OK')
+})
+
+
+app.listen(8080, () => {
+  console.log('App listening to port 8080')
+})
